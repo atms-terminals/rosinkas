ff95b307add1f086210ace201dcd91d2cdad1603
Sergey Lutsko <s.lutsko@gmail.com>
2016-12-07 15:07:16 +0600

    имитация проффита

---
+++ components/mockBalance.php
@@ -0,0 +1,149 @@
+<?php
+    return $mockBalance = array(
+        '64FA32000D' => array(
+            '@attributes' => array
+                (
+                    'ver' => '3',
+                    'direct' => '2',
+                    'lang' => 'RU',
+                ),
+            'answer' => array
+                (
+                    'CLIENT' => array
+                        (
+                            '@attributes' => array
+                                (
+                                    'ID_CARD' => '4',
+                                    'ID_CLIENT' => '6',
+                                    'NAME' => 'Маслова Е.Н.',
+                                )
+                        ),
+                    'ITEM' => array
+                        (
+                            '0' => array
+                                (
+                                    '@attributes' => array
+                                        (
+                                            'PURCHASE_DATE' => '10.10.2016',
+                                            'PURCHASE_SYMA' => '1800',
+                                            'PURCHASE_QTY' => '8',
+                                            'NAME' => 'Бас.Абонемент.Занянятие по плаванию (8-12 пос.)',
+                                            'ID' => '4',
+                                            'QTY' => '3',
+                                            'PRICE' => '225',
+                                            'UNIT' => '',
+                                            'ACTIVE' => '1',
+                                            'PAID' => '1',
+                                        )
+                                ),
+                            '1' => array
+                                (
+                                    '@attributes' => array
+                                        (
+                                            'PURCHASE_DATE' => '11.10.2016',
+                                            'PURCHASE_SYMA' => '1800',
+                                            'PURCHASE_QTY' => '8',
+                                            'NAME' => 'Бас.Абонемент.Занянятие по плаванию (8-12 пос.)',
+                                            'ID' => '4',
+                                            'QTY' => '0',
+                                            'PRICE' => '225',
+                                            'UNIT' => '',
+                                            'ACTIVE' => '0',
+                                            'PAID' => '1',
+                                        )
+                                ),
+                        ),
+                ),
+            'result' => array
+                (
+                    '@attributes' => array
+                        (
+                            'code' => '0',
+                            'text' => 'OK',
+                        )
+
+                ),
+        ),
+        '92FC820003' => array
+        (
+            '@attributes' => array
+                (
+                    'ver' => '3',
+                    'direct' => '2',
+                    'lang' => 'RU',
+                ),
+            'answer' => array
+                (
+                    'CLIENT' => array
+                        (
+                            '@attributes' => array
+                                (
+                                    'ID_CARD' => '1',
+                                    'ID_CLIENT' => '4',
+                                    'NAME' => 'Ковалев И.Ю.',
+                                )
+                        ),
+                    'ITEM' => array
+                        (
+                            '0' => array
+                                (
+                                    '@attributes' => array
+                                        (
+                                            'PURCHASE_DATE' => '10.10.2016',
+                                            'PURCHASE_SYMA' => '1800',
+                                            'PURCHASE_QTY' => '8',
+                                            'NAME' => 'Бас.Абонемент.Занянятие по плаванию (8-12 пос.)',
+                                            'ID' => '4',
+                                            'QTY' => '6',
+                                            'PRICE' => '225',
+                                            'UNIT' => '',
+                                            'ACTIVE' => '0',
+                                            'PAID' => '1',
+                                        )
+                                ),
+                            '1' => array
+                                (
+                                    '@attributes' => array
+                                        (
+                                            'PURCHASE_DATE' => '10.10.2016',
+                                            'PURCHASE_SYMA' => '100',
+                                            'PURCHASE_QTY' => '1',
+                                            'PURCHASE_FINISH' => '10.11.2016',
+                                            'NAME' => 'Бас.Разовое.Занянятие по плаванию (сотрудники)',
+                                            'ID' => '40',
+                                            'QTY' => '0',
+                                            'PRICE' => '100',
+                                            'UNIT' => '',
+                                            'ACTIVE' => '0',
+                                            'PAID' => '1',
+                                        )
+                                ),
+                            '2' => array
+                                (
+                                    '@attributes' => array
+                                        (
+                                            'PURCHASE_DATE' => '10.10.2016',
+                                            'PURCHASE_SYMA' => '1000',
+                                            'PURCHASE_QTY' => '1',
+                                            'PURCHASE_FINISH' => '10.11.2016',
+                                            'NAME' => 'Бас.Абонемент.Занянятие по плаванию (сотрудники) (12 занятий)',
+                                            'ID' => '41',
+                                            'QTY' => '12',
+                                            'PRICE' => '1000',
+                                            'UNIT' => '',
+                                            'ACTIVE' => '1',
+                                            'PAID' => '1',
+                                        )
+                                )
+                        )
+                ),
+            'result' => array
+                (
+                    '@attributes' => array
+                        (
+                            'code' => '0',
+                            'text' => 'OK',
+                        )
+                )
+        )
+    );
---
+++ components/mockServicesList.php
@@ -0,0 +1,440 @@
+<?php
+return array
+(
+    '@attributes' => array
+        (
+            'ver' => '3',
+            'direct' => '2',
+            'lang' => 'RU'
+        ),
+    'answer' => array
+        (
+            'ITEM' => array
+                (
+                    '0' => array
+                        (
+                            '@attributes' => array
+                                (
+                                    'ID' => '3',
+                                    'ID_UPPER' => '1',
+                                    'PRICE' => '1000',
+                                    'CNT_MIN' => '1',
+                                    'SROK' => '0',
+                                    'SROK_VID' => '0',
+                                    'UNIT' => ''
+                                ),
+                        ),
+                    '1' => array
+                        (
+                            '@attributes' => array
+                                (
+                                    'ID' => '4',
+                                    'ID_UPPER' => '1',
+                                    'PRICE' => '225',
+                                    'CNT_MIN' => '8',
+                                    'SROK' => '0',
+                                    'SROK_VID' => '0',
+                                    'UNIT' => ''
+                                ),
+                        ),
+                    '2' => array
+                        (
+                            '@attributes' => array
+                                (
+                                    'ID' => '41',
+                                    'ID_UPPER' => '1',
+                                    'PRICE' => '1000',
+                                    'CNT_MIN' => '1',
+                                    'SROK' => '1',
+                                    'SROK_VID' => '30',
+                                    'UNIT' => ''
+                                ),
+                        ),
+                    '3' => array
+                        (
+                            '@attributes' => array
+                                (
+                                    'ID' => '39',
+                                    'ID_UPPER' => '33',
+                                    'PRICE' => '5',
+                                    'CNT_MIN' => '1',
+                                    'SROK' => '0',
+                                    'SROK_VID' => '0',
+                                    'UNIT' => ''
+                                ),
+                        ),
+                    '4' => array
+                        (
+                            '@attributes' => array
+                                (
+                                    'ID' => '8',
+                                    'ID_UPPER' => '1',
+                                    'PRICE' => '350',
+                                    'CNT_MIN' => '8',
+                                    'SROK' => '0',
+                                    'SROK_VID' => '0',
+                                    'UNIT' => ''
+                                ),
+                        ),
+                    '5' => array
+                        (
+                            '@attributes' => array
+                                (
+                                    'ID' => '7',
+                                    'ID_UPPER' => '1',
+                                    'PRICE' => '400',
+                                    'CNT_MIN' => '1',
+                                    'SROK' => '0',
+                                    'SROK_VID' => '0',
+                                    'UNIT' => ''
+                                ),
+                        ),
+                    '6' => array
+                        (
+                            '@attributes' => array
+                                (
+                                    'ID' => '2',
+                                    'ID_UPPER' => '1',
+                                    'PRICE' => '260',
+                                    'CNT_MIN' => '1',
+                                    'SROK' => '1',
+                                    'SROK_VID' => '30',
+                                    'UNIT' => ''
+                                ),
+                        ),
+                    '7' => array
+                        (
+                            '@attributes' => array
+                                (
+                                    'ID' => '40',
+                                    'ID_UPPER' => '1',
+                                    'PRICE' => '100',
+                                    'CNT_MIN' => '1',
+                                    'SROK' => '1',
+                                    'SROK_VID' => '30',
+                                    'UNIT' => ''
+                                ),
+                        ),
+                    '8' => array
+                        (
+                            '@attributes' => array
+                                (
+                                    'ID' => '38',
+                                    'ID_UPPER' => '1',
+                                    'PRICE' => '300',
+                                    'CNT_MIN' => '1',
+                                    'SROK' => '1',
+                                    'SROK_VID' => '30',
+                                    'UNIT' => ''
+                                ),
+                        ),
+                    '9' => array
+                        (
+                            '@attributes' => array
+                                (
+                                    'ID' => '1',
+                                    'ID_UPPER' => '0',
+                                    'PRICE' => '0',
+                                    'CNT_MIN' => '1',
+                                    'SROK' => '0',
+                                    'SROK_VID' => '0',
+                                    'UNIT' => ''
+                                ),
+                        ),
+                    '10' => array
+                        (
+                            '@attributes' => array
+                                (
+                                    'ID' => '29',
+                                    'ID_UPPER' => '27',
+                                    'PRICE' => '200',
+                                    'CNT_MIN' => '1',
+                                    'SROK' => '0',
+                                    'SROK_VID' => '0',
+                                    'UNIT' => ''
+                                ),
+                        ),
+                    '11' => array
+                        (
+                            '@attributes' => array
+                                (
+                                    'ID' => '30',
+                                    'ID_UPPER' => '27',
+                                    'PRICE' => '11000',
+                                    'CNT_MIN' => '1',
+                                    'SROK' => '0',
+                                    'SROK_VID' => '0',
+                                    'UNIT' => ''
+                                ),
+                        ),
+                    '12' => array
+                        (
+                            '@attributes' => array
+                                (
+                                    'ID' => '31',
+                                    'ID_UPPER' => '27',
+                                    'PRICE' => '9000',
+                                    'CNT_MIN' => '1',
+                                    'SROK' => '0',
+                                    'SROK_VID' => '0',
+                                    'UNIT' => ''
+                                ),
+                        ),
+                    '13' => array
+                        (
+                            '@attributes' => array
+                                (
+                                    'ID' => '32',
+                                    'ID_UPPER' => '27',
+                                    'PRICE' => '13000',
+                                    'CNT_MIN' => '1',
+                                    'SROK' => '0',
+                                    'SROK_VID' => '0',
+                                    'UNIT' => ''
+                                ),
+                        ),
+                    '14' => array
+                        (
+                            '@attributes' => array
+                                (
+                                    'ID' => '36',
+                                    'ID_UPPER' => '27',
+                                    'PRICE' => '200',
+                                    'CNT_MIN' => '1',
+                                    'SROK' => '0',
+                                    'SROK_VID' => '0',
+                                    'UNIT' => ''
+                                ),
+                        ),
+                    '15' => array
+                        (
+                            '@attributes' => array
+                                (
+                                    'ID' => '28',
+                                    'ID_UPPER' => '27',
+                                    'PRICE' => '250',
+                                    'CNT_MIN' => '1',
+                                    'SROK' => '0',
+                                    'SROK_VID' => '0',
+                                    'UNIT' => ''
+                                ),
+                        ),
+                    '16' => array
+                        (
+                            '@attributes' => array
+                                (
+                                    'ID' => '27',
+                                    'ID_UPPER' => '0',
+                                    'PRICE' => '0',
+                                    'CNT_MIN' => '1',
+                                    'SROK' => '0',
+                                    'SROK_VID' => '0',
+                                    'UNIT' => ''
+                                ),
+                        ),
+                    '17' => array
+                        (
+                            '@attributes' => array
+                                (
+                                    'ID' => '34',
+                                    'ID_UPPER' => '33',
+                                    'PRICE' => '2000',
+                                    'CNT_MIN' => '1',
+                                    'SROK' => '0',
+                                    'SROK_VID' => '0',
+                                    'UNIT' => ''
+                                ),
+                        ),
+                    '18' => array
+                        (
+                            '@attributes' => array
+                                (
+                                    'ID' => '33',
+                                    'ID_UPPER' => '0',
+                                    'PRICE' => '0',
+                                    'CNT_MIN' => '1',
+                                    'SROK' => '0',
+                                    'SROK_VID' => '0',
+                                    'UNIT' => ''
+                                ),
+                        ),
+                    '19' => array
+                        (
+                            '@attributes' => array
+                                (
+                                    'ID' => '18',
+                                    'ID_UPPER' => '16',
+                                    'PRICE' => '225',
+                                    'CNT_MIN' => '1',
+                                    'SROK' => '0',
+                                    'SROK_VID' => '0',
+                                    'UNIT' => ''
+                                ),
+                        ),
+                    '20' => array
+                        (
+                            '@attributes' => array
+                                (
+                                    'ID' => '37',
+                                    'ID_UPPER' => '16',
+                                    'PRICE' => '300',
+                                    'CNT_MIN' => '1',
+                                    'SROK' => '0',
+                                    'SROK_VID' => '0',
+                                    'UNIT' => ''
+                                ),
+                        ),
+                    '21' => array
+                        (
+                            '@attributes' => array
+                                (
+                                    'ID' => '19',
+                                    'ID_UPPER' => '16',
+                                    'PRICE' => '2400',
+                                    'CNT_MIN' => '1',
+                                    'SROK' => '0',
+                                    'SROK_VID' => '0',
+                                    'UNIT' => ''
+                                ),
+                        ),
+                    '22' => array
+                        (
+                            '@attributes' => array
+                                (
+                                    'ID' => '17',
+                                    'ID_UPPER' => '16',
+                                    'PRICE' => '250',
+                                    'CNT_MIN' => '1',
+                                    'SROK' => '0',
+                                    'SROK_VID' => '0',
+                                    'UNIT' => ''
+                                ),
+                        ),
+                    '23' => array
+                        (
+                            '@attributes' => array
+                                (
+                                    'ID' => '16',
+                                    'ID_UPPER' => '0',
+                                    'PRICE' => '0',
+                                    'CNT_MIN' => '1',
+                                    'SROK' => '0',
+                                    'SROK_VID' => '0',
+                                    'UNIT' => ''
+                                ),
+                        ),
+                    '24' => array
+                        (
+                            '@attributes' => array
+                                (
+                                    'ID' => '22',
+                                    'ID_UPPER' => '20',
+                                    'PRICE' => '225',
+                                    'CNT_MIN' => '1',
+                                    'SROK' => '0',
+                                    'SROK_VID' => '0',
+                                    'UNIT' => ''
+                                ),
+                        ),
+                    '25' => array
+                        (
+                            '@attributes' => array
+                                (
+                                    'ID' => '24',
+                                    'ID_UPPER' => '20',
+                                    'PRICE' => '4500',
+                                    'CNT_MIN' => '1',
+                                    'SROK' => '0',
+                                    'SROK_VID' => '0',
+                                    'UNIT' => ''
+                                ),
+                        ),
+                    '26' => array
+                        (
+                            '@attributes' => array
+                                (
+                                    'ID' => '26',
+                                    'ID_UPPER' => '20',
+                                    'PRICE' => '450',
+                                    'CNT_MIN' => '1',
+                                    'SROK' => '0',
+                                    'SROK_VID' => '0',
+                                    'UNIT' => ''
+                                ),
+                        ),
+                    '27' => array
+                        (
+                            '@attributes' => array
+                                (
+                                    'ID' => '23',
+                                    'ID_UPPER' => '20',
+                                    'PRICE' => '2200',
+                                    'CNT_MIN' => '1',
+                                    'SROK' => '0',
+                                    'SROK_VID' => '0',
+                                    'UNIT' => ''
+                                ),
+                        ),
+                    '28' => array
+                        (
+                            '@attributes' => array
+                                (
+                                    'ID' => '21',
+                                    'ID_UPPER' => '20',
+                                    'PRICE' => '250',
+                                    'CNT_MIN' => '1',
+                                    'SROK' => '0',
+                                    'SROK_VID' => '0',
+                                    'UNIT' => ''
+                                ),
+                        ),
+                    '29' => array
+                        (
+                            '@attributes' => array
+                                (
+                                    'ID' => '25',
+                                    'ID_UPPER' => '20',
+                                    'PRICE' => '600',
+                                    'CNT_MIN' => '1',
+                                    'SROK' => '0',
+                                    'SROK_VID' => '0',
+                                    'UNIT' => ''
+                                ),
+                        ),
+                    '30' => array
+                        (
+                            '@attributes' => array
+                                (
+                                    'ID' => '20',
+                                    'ID_UPPER' => '0',
+                                    'PRICE' => '0',
+                                    'CNT_MIN' => '1',
+                                    'SROK' => '0',
+                                    'SROK_VID' => '0',
+                                    'UNIT' => ''
+                                ),
+                        ),
+                    '31' => array
+                        (
+                            '@attributes' => array
+                                (
+                                    'ID' => '35',
+                                    'ID_UPPER' => '33',
+                                    'PRICE' => '50',
+                                    'CNT_MIN' => '1',
+                                    'SROK' => '0',
+                                    'SROK_VID' => '0',
+                                    'UNIT' => ''
+                                ),
+                        ),
+                ),
+        ),
+    'result' => array
+        (
+            '@attributes' => array
+                (
+                    'code' => '0',
+                    'text' => 'OK'
+                ),
+        ),
+);
